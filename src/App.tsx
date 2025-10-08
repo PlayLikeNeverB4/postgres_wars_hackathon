13ef6e1fd386ce88ff524a33d2b5169cc935e1a9
George Marcus <george.marcus@databricks.com>
2025-10-08 12:50:40 +0200

    Progress!

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from 'react'
+
+import {
+  IconCheck,
+  IconCopy,
+} from "@tabler/icons-react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "@/components/ui/input-group"
+
+const PSQL_COMMAND = "psql 'postgresql://neondb_owner:npg_jS9oZer0mcNT@ep-holy-moon-a8hho9rg-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require'"
+
+const useCopyToClipboard = () => {
+  const [isCopied, setIsCopied] = useState(false)
+  const copyToClipboard = useCallback((content: string) => {
+    navigator.clipboard
+      .writeText(content)
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000);
+      })
+  }, [])
+
+  return {
+    copyToClipboard,
+    isCopied,
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { copyToClipboard, isCopied } = useCopyToClipboard()
+  const king = 'George';
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-screen h-screen dark bg-background text-foreground flex flex-col gap-6 justify-center items-center">
+      <h1 className="text-4xl">King of the hill: <span className="text-red-400">{king}</span></h1>
+      <div className="w-screen px-50">
+        <InputGroup>
+          <InputGroupInput value={PSQL_COMMAND} readOnly className="font-mono" />
+          <InputGroupAddon align="inline-end">
+            <InputGroupButton
+              aria-label="Copy"
+              title="Copy"
+              size="icon-xs"
+              onClick={() => {
+                copyToClipboard(PSQL_COMMAND)
+              }}
+            >
+              {isCopied ? <IconCheck /> : <IconCopy />}
+            </InputGroupButton>
+          </InputGroupAddon>
+        </InputGroup>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
